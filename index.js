fd66d9eb0ce57c831173b8802f9ee8c1ad04f26c
Agathius <p.akos1148@gmail.com>
2023-01-24 23:32:45 +0100

    enabled cors for hosted client, relocated api entry point

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ const express = require('express');
 const app = express();
 const bp = require('body-parser');
 const cors = require('cors');
-const sendEmail = require('../routes/sendEmail');
-const reCaptcha = require('../routes/reCaptcha');
+const sendEmail = require('./routes/sendEmail');
+const reCaptcha = require('./routes/reCaptcha');
 const port = 3000;
 
-//middleware
+//middlewares
 app.use(bp.json())
 app.use(bp.urlencoded({ extended: true }));
-// app.use(cors({ // allow cors for a specific origin 
-//   origin: 'https://palkoakos.onrender.com'
-// }))
+app.use(cors({ // allow cors for a specific origin 
+  origin: 'https://palkoakos.onrender.com'
+}))
 
 //routes
 app.use('/sendmail', sendEmail); // send email
